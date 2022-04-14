f44b6059ea01010161e3b961dad1829994593369
백승현 <songh1000@gmail.com>
2022-04-14 21:56:33 +0900

    백승현 : 랜덤스테이지 다시 생성

---
+++ GameEngineContents/RandomStage.cpp
@@ -0,0 +1,93 @@
+#include "RandomStage.h"
+#include <GameEngineBase/GameEngineWindow.h>
+#include <GameEngine/GameEngineRenderer.h>
+#include <GameEngineBase/GameEngineDebug.h>
+#include <GameEngine/GameEngineImage.h>
+
+// Static Var
+
+// Static Func
+
+// constructor destructor
+RandomStage::RandomStage()
+	: RoundStateValue_(3)
+	, MountainDirectionValue_(0)
+	, isCurve_(false)
+	, BackStage_(nullptr)
+	, LeftOcean_(nullptr)
+	, RightOcean_(nullptr)
+	, LeftGround_(nullptr)
+	, RightGround_(nullptr)
+	, CurveTiles_(nullptr)
+	, Mountain1_(nullptr)
+	, Mountain2_(nullptr)
+	, LeftGlacier_(nullptr)
+	, RightGlacier_(nullptr)
+{
+}
+
+RandomStage::~RandomStage()
+{
+}
+
+//member Func
+void RandomStage::Start()
+{
+	SetPosition(GameEngineWindow::GetScale().Half());
+	SetScale(GameEngineWindow::GetScale());
+
+	BackStage_ = CreateRenderer("BackStage.bmp", 100);
+	BackStage_->SetPivot({ 0,-48 });
+
+	LeftOcean_ = CreateRenderer(100, RenderPivot::LEFTTOP);
+	LeftOcean_->SetPivot({ -416, -32 });
+	LeftOcean_->CreateAnimation("OceanTiles.bmp", "LeftOcean", 0, 3, 0.2f);
+	LeftOcean_->ChangeAnimation("LeftOcean");
+
+	RightOcean_ = CreateRenderer(100, RenderPivot::LEFTTOP);
+	RightOcean_->SetPivot({ 0, -32 });
+	RightOcean_->CreateAnimation("OceanTiles.bmp", "RightOcean", 4, 7, 0.2f);
+	RightOcean_->ChangeAnimation("RightOcean");
+
+	LeftGround_ = CreateRenderer(100, RenderPivot::LEFTTOP);
+	LeftGround_->SetPivot({ -416, -32 });
+	LeftGround_->CreateAnimation("GroundTiles.bmp", "LeftGround", 0, 1, 0.2f);
+	LeftGround_->ChangeAnimation("LeftGround");
+
+	RightGround_ = CreateRenderer(100, RenderPivot::LEFTTOP);
+	RightGround_->SetPivot({ 0, -32 });
+	RightGround_->CreateAnimation("GroundTiles.bmp", "RightGround", 2, 3, 0.2f);
+	RightGround_->ChangeAnimation("RightGround");
+
+	CurveTiles_ = CreateRenderer("CurveTiles.bmp", 100, RenderPivot::LEFTTOP);
+	CurveTiles_->SetPivot({ -416, -32 });
+	// CurveTiles_->SetIndex(0);
+	// 0: letf ocean
+	// 1: right ocean
+	// 2: left ground
+	// 3: right ground
+
+	Mountain1_ = CreateRenderer("Mountain.bmp", 100, RenderPivot::LEFTTOP);
+	Mountain1_->SetPivot({ -832, -64 });
+
+	Mountain2_ = CreateRenderer("Mountain.bmp", 100, RenderPivot::LEFTTOP);
+	Mountain2_->SetPivot({ 0, -64 });
+
+	LeftGlacier_ = CreateRenderer("Glaciers.bmp", 100, RenderPivot::LEFTTOP);
+	LeftGlacier_->SetPivot({ -416, -64 });
+	LeftGlacier_->SetIndex(0);
+
+	RightGlacier_ = CreateRenderer("Glaciers.bmp", 100, RenderPivot::LEFTTOP);
+	RightGlacier_->SetPivot({ 0, -64 });
+	RightGlacier_->SetIndex(1);
+}
+
+void RandomStage::Update()
+{
+
+}
+
+void RandomStage::Render()
+{
+
+}
---
+++ GameEngineContents/RandomStage.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <GameEngine/GameEngineActor.h>
+#include <GameEngineBase/GameEngineRandom.h>
+
+// 분류 :
+// 용도 :
+// 설명 :
+class RandomStage : public GameEngineActor
+{
+private:	// member Var
+	GameEngineRenderer* BackStage_;
+	GameEngineRenderer* LeftOcean_;
+	GameEngineRenderer* RightOcean_;
+	GameEngineRenderer* LeftGround_;
+	GameEngineRenderer* RightGround_;
+	GameEngineRenderer* CurveTiles_;
+	GameEngineRenderer* Mountain1_;
+	GameEngineRenderer* Mountain2_;
+	GameEngineRenderer* LeftGlacier_;
+	GameEngineRenderer* RightGlacier_;
+
+	GameEngineRandom RandomValue_;
+
+	unsigned int RoundStateValue_; // 맵 상태
+	unsigned int MountainDirectionValue_; // 산 이동 방향 값 0==stay, 1==right, 2==left
+	bool isCurve_;
+
+
+public:
+	RandomStage(); // default constructor 디폴트 생성자
+	~RandomStage(); // default destructor 디폴트 소멸자
+
+public:		// delete constructor
+	RandomStage(const RandomStage& _Other) = delete; // default Copy constructor 디폴트 복사생성자
+	RandomStage(RandomStage&& _Other) noexcept = delete; // default RValue Copy constructor 디폴트 RValue 복사생성자
+
+public:		//delete operator
+	RandomStage& operator=(const RandomStage& _Other) = delete; // default Copy operator 디폴트 대입 연산자
+	RandomStage& operator=(const RandomStage&& _Other) noexcept = delete; // default RValue Copy operator 디폴트 RValue 대입연산자
+
+private: //member Func
+
+	void Start() override;
+	void Update() override;
+	void Render() override;
+};
+
