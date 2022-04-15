6bd1ca600fe4782c00e261a39c32f557338d969c
백승현 <songh1000@gmail.com>
2022-04-15 11:43:45 +0900

    백승현 : 사운드 주소 임시 주석함 (필요시 주석 해제)

--- GameEngineContents/AntarcticAdventure.cpp
+++ GameEngineContents/AntarcticAdventure.cpp
@@ -32,19 +32,19 @@ void AntarcticAdventure::GameInit()
 			GameEngineImageManager::GetInst()->Load(AllImageFileList[i].GetFullPath());
 		}
 	}
-	{
-		GameEngineDirectory ResourcesDir;
-		ResourcesDir.MoveParent("WinAPIPortfolio");
-		ResourcesDir.Move("Resources");
-		ResourcesDir.Move("Sound");
+	//{
+	//	GameEngineDirectory ResourcesDir;
+	//	ResourcesDir.MoveParent("WinAPIPortfolio");
+	//	ResourcesDir.Move("Resources");
+	//	ResourcesDir.Move("Sound");
 
-		std::vector<GameEngineFile> AllImageFileList = ResourcesDir.GetAllFile();
+	//	std::vector<GameEngineFile> AllImageFileList = ResourcesDir.GetAllFile();
 
-		for (size_t i = 0; i < AllImageFileList.size(); i++)
-		{
-			GameEngineSound::LoadRes(AllImageFileList[i].GetFullPath());
-		}
-	}
+	//	for (size_t i = 0; i < AllImageFileList.size(); i++)
+	//	{
+	//		GameEngineSound::LoadRes(AllImageFileList[i].GetFullPath());
+	//	}
+	//}
 
 	{
 		GameEngineImage* Image = GameEngineImageManager::GetInst()->Find("OceanTiles.bmp");
